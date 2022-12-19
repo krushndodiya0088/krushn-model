440452c1f36531a86b2b4613a9d5027eb4ce816d
Krushn <krushndodiya0088@gmail.com>
2022-12-19 13:05:18 +0530

    k8

---
+++ C/Assignment/q11(6).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,sum;
+    printf("\n\n\xb2\xb2\xb2 SUM ODD NUMBERS \xb2\xb2\xb2\n\n");
+    printf("enter number:");
+     scanf("%d",&n);
+      printf("enter number:");
+     scanf("%d",&i);
+     if (n%2!=0)
+     {
+       sum=i+n;
+       printf("sum is:%d",sum);
+     }
+     else
+    {
+       printf("invalide nub:%d",sum);
+     }
+}
