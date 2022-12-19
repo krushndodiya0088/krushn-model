921404f0f2216a693b4b57ef1263122eb91237b1
Krushn <krushndodiya0088@gmail.com>
2022-12-19 12:49:31 +0530

    k8

---
+++ C/Assignment/q11(5).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    int i,n,sum;
+    printf("enter  number:");
+     scanf("%d",&n);
+      printf("enter  number:");
+     scanf("%d",&i);
+     if (n%2==0)
+     {
+       sum=i+n;
+       printf("sum is:%d",sum);
+     }
+     else
+    {
+       printf("invalide nub:%d",sum);
+     }
+}
