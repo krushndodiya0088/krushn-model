cf0b926b6f3b01dab9b63f8adde805d57147c5d1
Krushn <krushndodiya0088@gmail.com>
2022-12-21 22:23:19 +0530

    k10

---
+++ C/Assignment/q(patern1).c
@@ -0,0 +1,15 @@
+#include<stdio.h> 
+ void main() 
+{ 
+int n=5; 
+int i,j; 
+    for(i=1;i<=n;i++) 
+{ 
+    for(j=1;j<=i;j++) 
+{ 
+    printf("%d",j%2); 
+} 
+printf("\n"); 
+} 
+    return 0; 
+}
